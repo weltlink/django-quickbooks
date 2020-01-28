23c9f0a424bfebfef08aa510427227522bfa8b3c
bedilbek <bedilbek@gmail.com>
2020-01-28 16:03:06 +0500

    Fix Realm does not exist on qbd task creation

--- django_quickbooks/signals/qbd_task.py
+++ django_quickbooks/signals/qbd_task.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.dispatch import receiver
 
 from django_quickbooks import get_qbd_task_model, QUICKBOOKS_ENUMS, get_realm_model
@@ -9,22 +10,26 @@ RealmModel = get_realm_model()
 
 @receiver(qbd_task_create)
 def create_qbd_task(sender, qb_operation, qb_resource, object_id, content_type, schema_name, instance=None, *args, **kwargs):
-    realm = RealmModel.objects.get(schema_name=schema_name)
-    data = dict(
-        qb_resource=qb_resource,
-        object_id=object_id,
-        content_type=content_type,
-        realm=realm
-    )
+    try:
+        realm = RealmModel.objects.get(schema_name=schema_name)
+        data = dict(
+            qb_resource=qb_resource,
+            object_id=object_id,
+            content_type=content_type,
+            realm=realm
+        )
 
-    if QBDTaskModel.objects \
-            .filter(qb_operation=qb_operation, **data) \
-            .count():
-        return
+        if QBDTaskModel.objects \
+                .filter(qb_operation=qb_operation, **data) \
+                .count():
+            return
 
-    if qb_operation == QUICKBOOKS_ENUMS.OPP_MOD and QBDTaskModel.objects \
-            .filter(qb_operation=QUICKBOOKS_ENUMS.OPP_ADD, **data) \
-            .count():
-        return
+        if qb_operation == QUICKBOOKS_ENUMS.OPP_MOD and QBDTaskModel.objects \
+                .filter(qb_operation=QUICKBOOKS_ENUMS.OPP_ADD, **data) \
+                .count():
+            return
 
-    QBDTaskModel.objects.create(qb_operation=qb_operation, **data)
+        QBDTaskModel.objects.create(qb_operation=qb_operation, **data)
+
+    except ObjectDoesNotExist:
+        pass
