5419ade2a8b2ce5ca0149f9e4ea01fc292305b36
hassaan <hassaanalansary@yahoo.com>
2020-02-17 00:47:14 +0200

    Revert "Update settings.py"
    This reverts commit b6fd7b1

--- django_quickbooks/settings.py
+++ django_quickbooks/settings.py
@@ -36,7 +36,7 @@ DEFAULTS = {
     'APP_ID': '',
     'APP_NAME': 'Some App',
     'APP_DESCRIPTION': 'Some App Description',
-    'QB_TYPE': 'QBFS', # just a test
+    'QB_TYPE': 'QBFS',
     'OWNER_ID': '{1ee58da6-3051-11ea-b499-9cda3ea7afc1}',
 
     'LOCAL_MODEL_CLASSES': {
